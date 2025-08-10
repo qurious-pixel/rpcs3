24777111deea57c0c9622bad03fdc5aef89332f2
qurious-pixel <62252937+qurious-pixel@users.noreply.github.com>
2025-08-10 08:54:00 -0700

    Update build-windows-clang-cl.ps1

--- .ci/build-windows-clang-cl.ps1
+++ .ci/build-windows-clang-cl.ps1
@@ -120,7 +120,7 @@ Write-Host "Running CMake configuration"
     -DUSE_DISCORD_RPC=ON `
     -DWITH_LLVM=ON `
     -DBUILD_LLVM=OFF `
-    -DSTATIC_LINK_LLVM=ON `
+    -DSTATIC_LINK_LLVM=OFF `
     -DBUILD_RPCS3_TESTS=OFF `
     -DRUN_RPCS3_TESTS=OFF
 Write-Host "CMake configuration complete"
