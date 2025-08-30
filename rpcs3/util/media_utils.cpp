342b927369d19ba41e95b3f587fb560e6c212e2b
qurious-pixel <62252937+qurious-pixel@users.noreply.github.com>
2025-08-30 06:24:00 -0700

    ffmpeg8

--- rpcs3/util/media_utils.cpp
+++ rpcs3/util/media_utils.cpp
@@ -15,7 +15,7 @@
 #endif
 extern "C" {
 #include "libavcodec/avcodec.h"
-#include "libavcodec/codec.h"
+#include "libavcodec/codec_internal.h"
 #include "libavformat/avformat.h"
 #include "libavutil/dict.h"
 #include "libavutil/opt.h"
