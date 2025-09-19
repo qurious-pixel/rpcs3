d467d5607cdc4b693b66e5b536ae36585d22732a
qurious-pixel <62252937+qurious-pixel@users.noreply.github.com>
2025-09-19 13:22:02 -0700

    LLVM\cmake\LLVMConfig.cmake

--- .ci/build-windows-clang-cl.ps1
+++ .ci/build-windows-clang-cl.ps1
@@ -121,7 +121,7 @@ Write-Host "Running CMake configuration"
     -DUSE_DISCORD_RPC=ON `
     -DWITH_LLVM=ON `
     -DBUILD_LLVM=OFF `
-    -DLLVM_DIR="C:\Program Files\LLVM" `
+    -DLLVM_DIR="C:\Program Files\LLVM\cmake\LLVMConfig.cmake" `
     -DBUILD_RPCS3_TESTS=OFF `
     -DRUN_RPCS3_TESTS=OFF
 Write-Host "CMake configuration complete"
