8474e342eddaa2fdd4223af61c8edc7e2bc01265
qurious-pixel <62252937+qurious-pixel@users.noreply.github.com>
2025-10-03 09:26:14 -0700

    build with switches

--- .ci/build-linux.sh
+++ .ci/build-linux.sh
@@ -6,19 +6,29 @@ shellcheck .ci/*.sh
 
 git config --global --add safe.directory '*'
 
+curl -LO https://ffmpeg.org/releases/ffmpeg-"$FFMPEG_VER".tar.gz
+        tar -xzf ffmpeg-"$FFMPEG_VER".tar.gz
+        cd ffmpeg-"$FFMPEG_VER"
+        ./configure --enable-pic --disable-doc --enable-runtime-cpudetect --disable-autodetect --disable-everything --disable-x86asm \
+--enable-decoder=aac --enable-decoder=aac_latm --enable-decoder=atrac3 --enable-decoder=atrac3p --enable-decoder=atrac9 --enable-decoder=mp3 --enable-decoder=pcm_s16le --enable-decoder=pcm_s8 \
+--enable-decoder=mov --enable-decoder=h264 --enable-decoder=mpeg4 --enable-decoder=mpeg2video --enable-decoder=mjpeg --enable-decoder=mjpegb \
+--enable-encoder=pcm_s16le --enable-encoder=mp3 --enable-encoder=ac3 --enable-encoder=aac \
+--enable-encoder=ffv1 --enable-encoder=mpeg4 --enable-encoder=mjpeg --enable-encoder=h264 \
+--enable-muxer=avi --enable-muxer=h264 --enable-muxer=mjpeg --enable-muxer=mp4 \
+--enable-demuxer=h264 --enable-demuxer=m4v --enable-demuxer=mp3 --enable-demuxer=mpegvideo --enable-demuxer=mpegps --enable-demuxer=mjpeg --enable-demuxer=mov --enable-demuxer=avi --enable-demuxer=aac --enable-demuxer=pmp --enable-demuxer=oma --enable-demuxer=pcm_s16le --enable-demuxer=pcm_s8 --enable-demuxer=wav \
+--enable-parser=h264 --enable-parser=mpeg4video --enable-parser=mpegaudio --enable-parser=mpegvideo --enable-parser=mjpeg --enable-parser=aac --enable-parser=aac_latm \
+ --enable-protocol=file --enable-bsf=mjpeg2jpeg --enable-indev=dshow
+        make
+        make install
+        ffmpeg -version
+        cd -
+
 # Pull all the submodules except some
 # Note: Tried to use git submodule status, but it takes over 20 seconds
 # shellcheck disable=SC2046
 git submodule -q update --init $(awk '/path/ && !/llvm/ && !/opencv/ && !/libsdl-org/ && !/curl/ && !/zlib/ { print $3 }' .gitmodules)
 
-curl -LO https://ffmpeg.org/releases/ffmpeg-"$FFMPEG_VER".tar.gz
-        tar -xzf ffmpeg-"$FFMPEG_VER".tar.gz
-        cd ffmpeg-"$FFMPEG_VER"
-        ./configure
-        make
-        make install
-        ffmpeg -version
-        cd -
+
 
 mkdir build && cd build || exit 1
 
