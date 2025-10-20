1e7f6a4a4ede9348de859949ed4501d3b1c0a96c
qurious-pixel <62252937+qurious-pixel@users.noreply.github.com>
2025-10-20 07:02:48 -0700

    Update build-mac.sh

--- .ci/build-mac.sh
+++ .ci/build-mac.sh
@@ -12,14 +12,14 @@ export HOMEBREW_NO_INSTALL_CLEANUP=1
 #arch -x86_64 /usr/local/bin/brew install -f --overwrite --quiet ffmpeg@5 "llvm@$LLVM_COMPILER_VER" glew sdl3 vulkan-headers
 #arch -x86_64 /usr/local/bin/brew link -f --overwrite --quiet "llvm@$LLVM_COMPILER_VER" ffmpeg@5
 
-brew install -f --overwrite --quiet fmt "llvm@$LLVM_COMPILER_VER"
-brew link -f --overwrite --quiet "llvm@$LLVM_COMPILER_VER"
+#brew install -f --overwrite --quiet fmt "llvm@$LLVM_COMPILER_VER"
+#brew link -f --overwrite --quiet "llvm@$LLVM_COMPILER_VER"
 rm /usr/local/bin/{idle3,pip3,pydoc3,python3,python3-config}
 ls -al /usr/local/bin/*3* || true
 arch -x86_64 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 arch -x86_64 /usr/local/bin/brew install -f --overwrite --quiet ccache pipenv ffmpeg@5 "llvm@$LLVM_COMPILER_VER" glew sdl3 vulkan-headers
 arch -x86_64 /usr/local/bin/brew link -f --overwrite --quiet "llvm@$LLVM_COMPILER_VER" ffmpeg@5
-brew unlink fmt && brew link fmt
+#brew unlink fmt && brew link fmt
 
 # moltenvk based on commit for 1.3.0 release
 export HOMEBREW_DEVELOPER=1 # Prevents blocking of local formulae
