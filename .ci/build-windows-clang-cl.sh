87d1a36a18af302453236fae51af4a855b66c3f6
qurious-pixel <62252937+qurious-pixel@users.noreply.github.com>
2025-10-21 16:59:53 -0700

    set -ex / remove echos

--- .ci/build-windows-clang-cl.sh
+++ .ci/build-windows-clang-cl.sh
@@ -1,9 +1,9 @@
-#!/bin/sh -e
+#!/bin/sh -ex
 
-echo "Starting RPCS3 build (Bash script)"
+#echo "Starting RPCS3 build (Bash script)"
 
 # Automatically find clang_rt.builtins-x86_64.lib
-echo "Searching for clang_rt.builtins-x86_64.lib ..."
+#echo "Searching for clang_rt.builtins-x86_64.lib ..."
 clangBuiltinsLibPath=$(find "C:\Program Files\LLVM\lib\clang" -name "clang_rt.builtins-x86_64.lib" | sed 's|Program Files|PROGRA~1|g')
 
 if [ -z "$clangBuiltinsLibPath" ]; then
@@ -16,11 +16,11 @@ clangBuiltinsLib=$(basename "$clangBuiltinsLibPath")
 # shellcheck disable=SC2028 
 clangPath=$(echo "C:\Program Files\LLVM\bin" | sed 's|Program Files|PROGRA~1|g')
 
-echo "Found Clang builtins library: $clangBuiltinsLib in $clangBuiltinsDir"
-echo "Found Clang Path: $clangPath"
+#echo "Found Clang builtins library: $clangBuiltinsLib in $clangBuiltinsDir"
+#echo "Found Clang Path: $clangPath"
 
 # Search for mt.exe in SDK bin directories
-echo "Searching for llvm-mt.exe ..."
+#echo "Searching for llvm-mt.exe ..."
 mtPath=$(find "$clangPath" -name "llvm-mt.exe")
 
 if [ -z "$mtPath" ]; then
@@ -28,7 +28,7 @@ if [ -z "$mtPath" ]; then
     exit 1
 fi
 
-echo "Found llvm-mt.exe at: $mtPath"
+#echo "Found llvm-mt.exe at: $mtPath"
 
 VcpkgRoot="$(pwd)/vcpkg"
 VcpkgTriplet="$VCPKG_TRIPLET"
@@ -37,22 +37,22 @@ VcpkgInclude="$VcpkgInstall/include"
 VcpkgLib="$VcpkgInstall/lib"
 
 # Configure git safe directory
-echo "Configuring git safe directory"
+#echo "Configuring git safe directory"
 git config --global --add safe.directory '*'
 
 # Initialize submodules except certain ones
-echo "Initializing submodules"
+#echo "Initializing submodules"
 # shellcheck disable=SC2046
 git submodule -q update --init $(awk '/path/ && !/llvm/ && !/opencv/ && !/FAudio/ && !/libpng/ && !/zlib/ && !/feralinteractive/ { print $3 }' .gitmodules)
 
 # Create and enter build directory
-echo "Creating build directory"
+#echo "Creating build directory"
 mkdir -p build
 cd build || exit
-echo "Changed directory to: $(pwd)"
+#echo "Changed directory to: $(pwd)"
 
 # Run CMake with Ninja generator and required flags
-echo "Running CMake configuration"
+#echo "Running CMake configuration"
 cmake .. \
     -G Ninja \
     -DCMAKE_BUILD_TYPE=Release \
@@ -86,14 +86,14 @@ cmake .. \
     -DSTATIC_LINK_LLVM=ON \
     -DBUILD_RPCS3_TESTS=OFF
 
-echo "CMake configuration complete"
+#echo "CMake configuration complete"
 
 # Build with ninja
-echo "Starting build with Ninja..."
+#echo "Starting build with Ninja..."
 ninja
 
-echo "Build succeeded"
+#echo "Build succeeded"
 
 # Go back to root directory
 cd ..
-echo "Returned to root directory: $(pwd)"
+#echo "Returned to root directory: $(pwd)"
