28585b002064c65183ba3fb387aaaca66516b173
qurious-pixel <62252937+qurious-pixel@users.noreply.github.com>
2025-11-22 12:12:44 -0800

    ls -l /Library/Developer/CommandLineTools/SDKs/

--- .ci/build-mac-arm64.sh
+++ .ci/build-mac-arm64.sh
@@ -6,6 +6,8 @@ export HOMEBREW_NO_INSTALLED_DEPENDENTS_CHECK=1
 export HOMEBREW_NO_ENV_HINTS=1
 export HOMEBREW_NO_INSTALL_CLEANUP=1
 
+ls -l /Library/Developer/CommandLineTools/SDKs/
+
 sudo xcode-select --switch /Library/Developer/CommandLineTools
 brew install -f --overwrite --quiet pipenv googletest ffmpeg@5 "llvm@$LLVM_COMPILER_VER" glew sdl3 vulkan-headers
 brew link -f --quiet "llvm@$LLVM_COMPILER_VER" ffmpeg@5
